de5b2d63ef698b2494560976993d38e32880529e
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-18 16:32:24 -0300

    create lesson dto

---
+++ src/main/java/com/gleidsonsilva/crud_spring/dto/LessonDTO.java
@@ -0,0 +1,11 @@
+package com.gleidsonsilva.crud_spring.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record LessonDTO(
+        Long id,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+        @NotNull @NotBlank @Length(min = 10, max = 100)  String lessonUrl) {
+}
