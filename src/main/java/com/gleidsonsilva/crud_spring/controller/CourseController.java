76c325db43dbd3d9c285d03adcc68a3a89a433ce
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-18 16:31:11 -0300

    create course controller class

---
+++ src/main/java/com/gleidsonsilva/crud_spring/controller/CourseController.java
@@ -0,0 +1,59 @@
+package com.gleidsonsilva.crud_spring.controller;
+
+import com.gleidsonsilva.crud_spring.dto.CourseDTO;
+import com.gleidsonsilva.crud_spring.service.CourseService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> list() {
+        return courseService.list();
+    }
+
+    @GetMapping("/{id}")
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO courseDTO) {
+        return courseService.create(courseDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid @NotNull CourseDTO courseDTO) {
+        return courseService.update(id, courseDTO);
+    }
+
+    @PutMapping("/{id}/tests")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateForTests(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid @NotNull CourseDTO courseDTO) {
+        courseService.updateForTests(id, courseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        courseService.delete(id);
+    }
+
+}
