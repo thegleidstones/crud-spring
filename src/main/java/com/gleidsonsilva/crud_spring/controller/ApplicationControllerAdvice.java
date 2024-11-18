c9fbd2e0d30981b3caca763344fb99e720c43b00
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-18 16:32:57 -0300

    create controller advice

---
+++ src/main/java/com/gleidsonsilva/crud_spring/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.gleidsonsilva.crud_spring.controller;
+
+import com.gleidsonsilva.crud_spring.exception.RecordNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleRecordNotFoundException(RecordNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
+                .reduce("", (acc, error) -> acc + error + "\n");
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations().stream()
+                .map((error -> error.getPropertyPath() + " " + error.getMessage()))
+                .reduce("", (acc, error) -> acc + error + "\n");
+    }
+
+}
